0cbdf0880f1346f41a400eb1c8e65ac98007e773
NM <nico.maretti@gmail.com>
2023-10-17 14:21:49 +0200

    Refactoring query per Favorites, SWM, SBM

--- app/Models/FileShare.php
+++ app/Models/FileShare.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FileShare extends Model
 {
@@ -26,5 +27,44 @@ class FileShare extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function getSharedByMe(): Collection
+    {
+        return FileShare::query()
+            ->with(['user', 'file.user'])
+            ->whereRelation('file.user', 'id', Auth::id())
+            ->get()
+            ->map(function (FileShare $sharedFile) {
+                $file['id'] = $sharedFile->file->id;
+                $file['name'] = $sharedFile->file->name;
+                $file['is_folder'] = $sharedFile->file->is_folder;
+                $file['is_favorite'] = !!$sharedFile->file->starred;
+                $file['mime_type'] = $sharedFile->file->mime_type;
+                $file['shared_with'] = $sharedFile->user->name;
+                $file['owner'] = '';
+                return $file;
+            })
+            ->sortBy(['name', 'shared_with']);
+    }
+
+    public static function getSharedWithMe(): Collection
+    {
+        return FileShare::query()
+            ->where('user_id', Auth::id())
+            ->with(['file', 'file.user'])
+            ->whereRelation('file', 'created_by', '!=', Auth::id())
+            ->get()
+            ->map(function (FileShare $sharedFile) {
+                $file['id'] = $sharedFile->file->id;
+                $file['name'] = $sharedFile->file->name;
+                $file['is_folder'] = $sharedFile->file->is_folder;
+                $file['is_favorite'] = !!$sharedFile->file->starred;
+                $file['mime_type'] = $sharedFile->file->mime_type;
+                $file['owner'] = $sharedFile->file->user->name;
+                $file['shared_with'] = '';
+                return $file;
+            })
+            ->sortBy(['name', 'owner']);
     }
 }
--- app/Models/StarredFile.php
+++ app/Models/StarredFile.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StarredFile extends Model
 {
@@ -23,5 +25,17 @@ class StarredFile extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function getFavorites(): Collection
+    {
+        return StarredFile::query()
+            ->with('file')
+            ->where('user_id', Auth::id())
+            ->get()
+            ->map(function (StarredFile $starredFile) {
+                return $starredFile->file;
+            })
+            ->sortBy('name');
     }
 }
